a8cec12b70a52043299479ff737462769c1f574d
James-Livesey <jamesliveg@gmail.com>
2024-11-09 22:40:49 +0000

    Fix a few issues to get working correctly on ARM64

--- makeiso.sh
+++ makeiso.sh
@@ -16,13 +16,30 @@ fi
 
 ./unmount.sh
 
-if [ $PLATFORM = "x86_64" ]; then
+GRUB_DIR_ARGS=""
+
+if [ $PLATFORM = "x86_64" ] || [ $PLATFORM = "arm64" ]; then
+    # Install EFI bootloader
+
     sudo losetup -P /dev/loop0 build/$PLATFORM/system.img
 
     sudo mount /dev/loop0p1 build/$PLATFORM/bootfs
     sudo mount /dev/loop0p2 build/$PLATFORM/rootfs
 
-    sudo grub-install --target=x86_64-efi --efi-directory=build/$PLATFORM/bootfs --boot-directory=build/$PLATFORM/rootfs/boot --removable
+    if [ $PLATFORM = "arm64" ]; then
+        # Download ARM64 copy of GRUB EFI files on x86-64 host
+
+        wget -nc http://ftp.uk.debian.org/debian/pool/main/g/grub2/grub-efi-arm64-bin_2.06-13+deb12u1_arm64.deb -O cache/$PLATFORM/grub-efi-arm64-bin.deb
+
+        if [ ! -e cache/$PLATFORM/grub-efi-arm64-bin ]; then
+            dpkg-deb -R cache/$PLATFORM/grub-efi-arm64-bin.deb cache/$PLATFORM/grub-efi-arm64-bin
+        fi
+
+        TARGET_ARGS="--target=arm64-efi --directory=cache/$PLATFORM/grub-efi-arm64-bin/usr/lib/grub/arm64-efi"
+        GRUB_DIR_ARGS="--directory=build/$PLATFORM/rootfs/$GRUB_LOCATION"
+    fi
+
+    sudo grub-install $TARGET_ARGS --efi-directory=build/$PLATFORM/bootfs --boot-directory=build/$PLATFORM/rootfs/boot --removable
 fi
 
 ./mount.sh
@@ -33,6 +50,7 @@ sudo cp host/$PLATFORM/initoverlay.sh build/$PLATFORM/rootfs/sbin/initoverlay
 
 sudo grub-mkrescue -o build/$PLATFORM/system.iso build/$PLATFORM/rootfs \
     --modules="part_msdos part_gpt normal linux configfile search" \
+    $GRUB_DIR_ARGS \
     -- \
     -volid LiveG-OS-IM \
     -chmod a+rwx,g-w,o-w,ug+s,+t,g-s,-t /usr/bin/sudo -- \
