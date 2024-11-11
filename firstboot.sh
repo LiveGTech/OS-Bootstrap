26d284bdc41a0c5b6a56f5ff2db7bd7f21d9ee79
James-Livesey <jamesliveg@gmail.com>
2024-11-11 21:35:47 +0000

    Fix booting not working on Pi 4

--- firstboot.sh
+++ firstboot.sh
@@ -247,7 +247,6 @@ cp -a /host/common/plymouth/. /usr/share/plymouth/themes/liveg/
 cp /host/common/plymouthd.conf /etc/plymouth/plymouthd.conf
 
 if [ $PLATFORM = "rpi" ]; then
-    sed -i -e "s/console=tty1 //g" /boot/firmware/cmdline.txt
     sed -i "1{s/$/quiet splash logo.nologo loglevel=3 systemd.show_status=auto rd.udev.log_level=3 vt.global_cursor_default=0 plymouth.ignore-serial-consoles/}" /boot/firmware/cmdline.txt
 fi
 
